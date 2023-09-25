3b207034cd9341e7515567380c6af547b7065ef1
sc4r3d0ntf34r <sc4r3d0ntf34r@localhost.localdomain>
2023-09-25 03:06:36 -0400

    Added basic options for connecting to ftpserver

--- dtp.py
+++ dtp.py
@@ -1,3 +1,4 @@
+from ftplib import FTP
 import argparse
 from os import walk
 from os import path
@@ -23,13 +24,25 @@ if __name__ == '__main__':
 		epilog="Made by scaredontfear: sc4r3d0ntf34r@tuta.io"
 		)
 
-	parser.add_argument("-D", "--dir", help="The directory to send.")
+	parser.add_argument("-d", "--dir", help="The directory to send.")
+	parser.add_argument("-u", "--username", help="the username to connect to the ftp server")
+	parser.add_argument("-A", "--anon", help="Login anonymously to ftp server", action="store_true")
 
 	args = parser.parse_args()
 
 	if(not path.isdir(args.dir)):
 		print("Directory invalid exiting...")
 		exit(2)
+
+	if(args.anon):
+		print("Logging in as anonymous user.")
+		user = "anonymous"
+	elif(args.username):
+		print(f"logging in as {args.username}")
+		user = args.username
+	else:
+		print("No user specified exiting...")
+		exit(3)
 
 	structure = get_tree(args.dir)
 
